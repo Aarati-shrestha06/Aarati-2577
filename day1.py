268d291df680dbed88eb258a5505efa260d77239
Aarati-shrestha06 <aaratishrestha072@gmail.com>
2025-07-10 12:36:05 +0545

    professional course

---
+++ day1.py
@@ -0,0 +1,26 @@
+#modules 
+# built in module
+#2. external module
+
+print("Hello world")
+
+
+#comments
+'''
+this is multiline comments
+
+'''
+# TO PRINT THE TWINKLE TWINKLE POEM
+
+print("Twinkle, twinkle, little star,")
+print("    How I wonder what you are!")
+print("        Up above the world so high,")
+print("        Like a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("    How I wonder what you are")
+
+
+
+
+
+
