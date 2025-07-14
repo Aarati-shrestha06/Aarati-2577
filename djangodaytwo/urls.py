47a33f768a5df7e1f3aa982981eaaf2fd5baa7d9
Aarati-shrestha06 <aaratishrestha072@gmail.com>
2025-07-14 13:55:18 +0545

    hello

--- djangodaytwo/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.urls import path
-
-from djangodaytwo import views
-
-urlpatterns = [
-    path('home/', views.home),
-    path('about/', views.about),
-]
--- djangodaytwo/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return HttpResponse("hello")
-def about(request):
-    return HttpResponse("this is about page")
- 
